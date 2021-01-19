71c6e05f38b1c3eaad8315a28ea0c6471ee63aa7
Jasmin Kaset <kasetski@gmail.com>
2021-01-19 14:44:42 -0600

    update animals works

--- request_handler.py
+++ request_handler.py
@@ -175,7 +175,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write("".encode())
 
     def do_PUT(self):
-        self._set_headers(204)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
@@ -183,18 +182,42 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-    # Delete a single animal from the list
-        if resource == "animals":
-            update_animal(id, post_body)
-        if resource == "customers":
-            update_customer(id, post_body)
-        if resource == "employees":
-            update_employee(id, post_body)
-        if resource == "locations":
-            update_location(id, post_body)
+        success = False
 
-    # Encode the new animal and send in response
+        if resource == "animals":
+            success = update_animal(id, post_body)
+    # rest of the elif's
+        elif resource == "customers":
+            success = update_customer(id, post_body)
+    #         update_customer(id, post_body)
+
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
+
         self.wfile.write("".encode())
+    # def do_PUT(self):
+    #     self._set_headers(204)
+    #     content_len = int(self.headers.get('content-length', 0))
+    #     post_body = self.rfile.read(content_len)
+    #     post_body = json.loads(post_body)
+
+    # # Parse the URL
+    #     (resource, id) = self.parse_url(self.path)
+
+    # # Delete a single animal from the list
+    #     if resource == "animals":
+    #         update_animal(id, post_body)
+    #     if resource == "customers":
+    #         update_customer(id, post_body)
+    #     if resource == "employees":
+    #         update_employee(id, post_body)
+    #     if resource == "locations":
+    #         update_location(id, post_body)
+
+    # # Encode the new animal and send in response
+    #     self.wfile.write("".encode())
 # This function is not inside the class. It is the starting
 # point of this application.
 def main():
